7b6d2d003030d3edb423978d15fa6f399955c9bc
Noel La Borde <138098592+noel-la-borde@users.noreply.github.com>
2026-03-23 20:38:43 -0400

    Stack instructions changes.

---
+++ src/main/java/cpu.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+class cpu {
+
+    static int getDataRegister(int dn) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    static class ccr {
+
+        public ccr() {
+        }
+    }
+
+}
